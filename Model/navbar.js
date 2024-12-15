034053b91f082ba5e30d5e38ff341ac8a06a784d
Jitendra Devra <jitudevra02468@gmail.com>
2024-12-15 22:03:07 +0530

    first commit

---
+++ Model/navbar.js
@@ -0,0 +1,19 @@
+// const profileIcon = document.getElementById('profileIcon');
+//     const loginForm = document.getElementById('loginForm');
+//     const overlay = document.getElementById('overlay');
+
+//     // Show login form
+//     profileIcon.addEventListener('click', () => {
+//       loginForm.classList.add('active');
+//       overlay.classList.add('active');
+//     });
+
+//     // Hide login form when clicking outside
+//     overlay.addEventListener('click', () => {
+//       loginForm.classList.remove('active');
+//       overlay.classList.remove('active');
+//     });
+
+  function redirectToLogin() {
+    window.location.href = 'Login.html';
+  }
